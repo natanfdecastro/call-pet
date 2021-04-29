55fc56106dcf1155b531b099f6a017369ab81ff7
Natán Fernández de Castro <natanfdecastro.tec@gmail.com>
2021-04-28 23:02:39 -0600

    [+] tabla visualizar datos de pedidos

--- schema/Pedido.js
+++ schema/Pedido.js
@@ -16,18 +16,30 @@ cube(`Pedido`, {
     id: {
       sql: `id`,
       type: `number`,
-      primaryKey: true
+      primaryKey: true,
+      shown: true
+    },
+
+    tiempo_aviso: {
+      sql: `tiempo_aviso`,
+      type: `number`, 
+    },
+
+    consumo_dias: {
+      sql: `consumo_dias`,
+      type: `string`,
     },
     
-    fechaCompra: {
+    fecha_compra: {
       sql: `fecha_compra`,
       type: `time`
     },
     
-    fechaVencimiento: {
+    fecha_vencimiento: {
       sql: `fecha_vencimiento`,
       type: `time`
     }
+    
   },
   
   dataSource: `default`
